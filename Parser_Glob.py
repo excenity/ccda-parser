4ad42df57ea3abdb22d7dbd7898ec51f667193dc
Kevin Yu <k.yu@northwestern.edu>
2019-07-11 16:59:47 -0500

    Update Parser_Glob.py
    Allot for some type of blanks such as <td/> <td rowspans>

--- Parser_Glob.py
+++ Parser_Glob.py
@@ -1,16 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Newest XML Parser Tool
+
+For AMIA Submission 
+"""
+# %%  prep
 from xml.dom.minidom import parse
 import pandas as pd
 import lxml.etree as et
 import glob
+import os 
+import re
 
-#Set Up Working Directory + Associated Variables + prefixes for lxml
-folderPath = "/Users/Excenity/Documents/CHiP-LOCAL/H3/XML_Parser/Files/test-18"
+# Set up working directory 
+os.chdir('C:/Users/jyk306/Documents/CHiP/C-CDA Parser/output')
+
+# Set Up Working Directory + Associated Variables + prefixes for lxml
+folderPath = "C:/Users/jyk306/Documents/CHiP/C-CDA Parser/Proto Files"
 filePaths = glob.glob(folderPath+"/*")
 prefix = '{urn:hl7-org:v3}'
 
-def getCurrPtId():
-    # Gets the current patient ID
+# %% swap out blank datapoints
 
+f_open = open('C:/Users/jyk306/Documents/CHiP/C-CDA Parser/Test Files/1.xml')
+xml_file = f_open.read()
+f_open.close()
+
+xml_file = re.sub('<td/>', '<td>NA</td>', xml_file)
+xml_file = re.sub('<td rowspan="1"/>', '<td>NA</td>', xml_file)
+
+f_open = open('1_new.xml', 'w+')
+f_open.write(xml_file)
+f_open.close()
+
+# %% functions 
+def getCurrPtId():
+    # get patient id 
+    
     global ptFound 
     ptFound = False
     global ptId
@@ -25,7 +51,8 @@ def getCurrPtId():
     return ptId
  
 def tableNames():
-
+    # get the table names in the C-CDA file
+    
     titles = []
 
     for section in currPt.getElementsByTagName("section"):
@@ -37,10 +64,9 @@ def tableNames():
         for node in nodes:
             if node.nodeType == node.TEXT_NODE:
                 print(node.data)
-                
-    return titles
-
+    
 def getTable(table_name):
+    # get the relevant table for the file 
         
     global currPt 
     global key_table
@@ -58,6 +84,7 @@ def getTable(table_name):
     return key_table
 
 def printTableName(table):
+    # get the table of interest based on name
     
     for title in table.getElementsByTagName("title"):
         nodes = title.childNodes
@@ -66,6 +93,7 @@ def printTableName(table):
                 print(node.data)
 
 def getHeaders(table):
+    # get headers of the table of interest
     
     headerList = []
     
@@ -79,6 +107,7 @@ def getHeaders(table):
     return headerList
 
 def getData(table):
+    # get data of the table of interest
     
     dataList = []
             
@@ -87,12 +116,14 @@ def getData(table):
         for node in nodes:
             if node.nodeType == node.TEXT_NODE:
                 dataList.append(node.data)
-            
+            else:
+                dataList.append('NA')
     
     #print(dataList)        
     return dataList
 
 def chunker(dataList, headers):
+    # creates a new row for every new row of data based on length of data
     
     DataLL = []
     DataChunk =[]
@@ -107,6 +138,9 @@ def chunker(dataList, headers):
     return DataLL
 
 def dataFrame(table_name):
+    # creates the final dataframe for the patient's table of interest
+    
+    global tableData
     
     key_table = getTable(table_name)
     
@@ -121,13 +155,15 @@ def dataFrame(table_name):
     return df
 
 def getAllPatients(table_name):
+    # puts everything together 
 
     global currFile
     global tree
     global root
     global currPt
     global ptId
-    global ptFound
+    global ptFound, df_row
+
 
     dfs = []
     
@@ -157,12 +193,30 @@ def getAllPatients(table_name):
     
     return table_final
 
+# %% run functions
 def main():
     
-    table_f = getAllPatients("Encounters")
+    table_f = getAllPatients("Results")
     print(table_f)
-    table_f.to_csv("Encounters-18.csv")
+    table_f.to_csv("test.csv")
     
+    return 
+
 main()
 
+# %% test 
+
+labs = getTable('Results')
+
+dataList = []
+n = 0
+
+for data in labs.getElementsByTagName("td"):
+        nodes = data.childNodes
+        for node in nodes:
+            n = n + 1
+            if node.nodeType == node.TEXT_NODE:
+                dataList.append(node.data)
+            else:
+                dataList.append('NA')
 
