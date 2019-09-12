9cba62571e82ad88740d4fb4618c0ee941efa105
Kevin Yu <k.yu@northwestern.edu>
2019-09-12 10:26:53 -0500

    update with data layer

--- Data_Layer.py
+++ Data_Layer.py
@@ -9,9 +9,11 @@ import os
 os.chdir('C:/Users/jyk306/Documents/CHiP/C-CDA Parser/git/')
 
 # %% import data 
+
 import Parser_Glob as PG
 import pandas as pd
 import re
+
 # %% get data 
 
 demographics, encounters, problemList, vitals, results, meds = PG.inputTables_simple()
@@ -27,4 +29,16 @@ vitals.Vital_Value = vitals.Vital_Value.str.replace(r'\D+', '').astype('int')
 vitals = vitals.groupby(['pt_id', 'Date', 'Vital_Name'])['Vital_Value'].mean().unstack(fill_value = 0)
 vitals = vitals.reset_index()
 
-# %% 
+# %% meds
+
+# parse RXnorm Codes
+meds['rxnorm'] = meds.RXnormCode.apply(lambda x: re.findall('\d+', x))
+meds.rxnorm = meds.rxnorm.astype('str')
+
+# conver med names to upper case
+meds.GenericName = meds.GenericName.str.upper()
+
+# get dosage information 
+meds['dosage'] = meds.ProductStrength.apply(lambda x: re.findall('\d+', x))
+meds.dosage = meds['dosage']
+
